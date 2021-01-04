657f8a2e922d31d53821907bae3345251a962161
CherishYii <cherish@cherish.pw>
2021-01-04 18:13:32 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,75 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+
+// 导入需要的组件
+// import Films from '@/views/Films/Index'
+// import NowPlaying from "@/views/Films/NowPlaying";
+// import ComingSoon from "@/views/Films/ComingSoon";
+// import Detail from "@/views/Films/Detail";
+// import Cinemas from "@/views/Cinemas/Index";
+// import Center from "@/views/Center/Index";
+
+// 导入经过模块化的路由
+import filmRouter from "./routes/film";
+import cinemaRouter from "./routes/cinema";
+import centerRouter from "./routes/center";
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    {
+        path: "/",
+        redirect: "/films/nowPlaying",
+    },
+    // 模块化
+    // 需要展开的，导出的是一个数组，但是人家希望要的是对象
+    ...filmRouter,
+    cinemaRouter,
+    centerRouter,
+    //     电影模块
+    // 	        电影模块父			/films
+    // 		    正在热映			/films/nowPlaying
+    // 		    即将上映			/films/comingSoon
+    // 	    电影详情			/film/:filmId
+    // {
+    //     path: "/films",
+    //     component: Films,
+    //     children: [
+    //         { path: "nowPlaying", component: NowPlaying },
+    //         { path: "comingSoon", component: ComingSoon },
+    //     ],
+    // },
+    // {
+    //     path: "/film/:filmId",
+    //     component: Detail,
+    // },
+    // 影院模块
+    // 	/cinemas
+    // {
+    //     path: "/cinemas",
+    //     component: Cinemas,
+    // },
+    // 我的模块
+    // 	/center
+    // {
+    //     path: "/center",
+    //     component: Center,
+    // },
+    //   {
+    //     path: '/about',
+    //     name: 'About',
+    //     // route level code-splitting
+    //     // this generates a separate chunk (about.[hash].js) for this route
+    //     // which is lazy-loaded when the route is visited.
+    //     // 坑：回头再获取路由信息的时候可能会出错（异步加载路由）
+    //     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    //   }
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
+});
 
-export default router
+export default router;
